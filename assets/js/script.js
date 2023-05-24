b6156d5cea34ed51c7c616ec4dc2c7759704df9f
TKuhner <trevorkuhner29@gmail.com>
2023-05-24 18:46:49 -0500

    added local storage save for textarea key and value

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,48 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-  // TODO:
-  // Add a listener for click events on the save button. 
+// TODO:
+// Add a listener for click events on the save button. 
 
-  // This code should use the id in the containing time-block as a key to save the user input in local storage. 
+// This code should use the id in the containing time-block as a key to save the user input in local storage. 
 
-  // HINT: What does `this` reference in the click listener function? 
+// HINT: What does `this` reference in the click listener function? 
 
-  // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
+// How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
 
-  // How might the id be useful when saving the description in local storage?
+// How might the id be useful when saving the description in local storage?
 
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
+// TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
 
-  // HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? 
+// HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? 
 
-  // How can Day.js be used to get the current hour in 24-hour time?
+// How can Day.js be used to get the current hour in 24-hour time?
 
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. 
+// TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. 
 
-  // HINT: How can the id attribute of each time-block be used to do this?
+// HINT: How can the id attribute of each time-block be used to do this?
 
-  // TODO: Add code to display the current date in the header of the page.
+// TODO: Add code to display the current date in the header of the page.
+
+
 
 
 var today = dayjs();
-
-
+var saveBtn = document.querySelector(".saveBtn");
 
 
 $('#currentDay').text(today.format('MMM D, YYYY, h:mm:ss a'));
+
+
+
+// save text in textarea to local storage
+$(".saveBtn").on("click", function () {
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+
+  localStorage.setItem(time, text);
+
+  console.log(text, time);
+});
 
 
