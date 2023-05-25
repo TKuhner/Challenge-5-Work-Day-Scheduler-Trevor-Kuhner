618c60cd252770a66a9ead91dd1f0a7d08e92290
TKuhner <trevorkuhner29@gmail.com>
2023-05-24 21:39:57 -0500

    changed text colors to black so they're more readable

---
+++ README.md
@@ -0,0 +1,33 @@
+Work Day Scheduler
+
+A simple calendar app for scheduling your work day.
+
+The Work Day Scheduler is a web application that allows users to plan and organize their work day by providing a time-block interface for each hour. Users can enter their tasks or appointments in the corresponding time blocks and save them for future reference. The interface also dynamically updates the color of each time block to indicate whether it is in the past, present, or future based on the current time.
+Features
+
+    Display of current date and time at the top of the page.
+    Time blocks for each hour of the work day.
+    Ability to enter and save tasks or appointments in the time blocks.
+    Automatic color-coding of time blocks based on the current hour.
+    Option to clear all saved tasks and appointments.
+
+Technologies Used
+
+    HTML
+    CSS (Bootstrap)
+    JavaScript (jQuery)
+    Day.js
+    Local Storage
+
+Getting Started
+
+To use the Work Day Scheduler, follow these steps:
+
+    Clone the repository: git clone https://github.com/your-username/work-day-scheduler.git
+    Open the index.html file in your web browser.
+    The current date and time will be displayed at the top of the page.
+    Each time block represents an hour of the work day.
+    Enter your tasks or appointments in the corresponding time blocks.
+    Click the save button to save your entries.
+    The time blocks will be color-coded to indicate whether they are in the past, present, or future based on the current time.
+    To clear all saved entries, click the "Clear Storage" button.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,80 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
 // TODO:
 // Add a listener for click events on the save button. 
-
 // This code should use the id in the containing time-block as a key to save the user input in local storage. 
-
 // HINT: What does `this` reference in the click listener function? 
-
 // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
-
 // How might the id be useful when saving the description in local storage?
-
 // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
-
 // HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? 
-
 // How can Day.js be used to get the current hour in 24-hour time?
-
 // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. 
-
 // HINT: How can the id attribute of each time-block be used to do this?
-
 // TODO: Add code to display the current date in the header of the page.
 
 
-var today = dayjs();
-var saveBtn = document.querySelector(".saveBtn");
-var hoursEl = document.querySelectorAll(".time-block");
+// Get date and time 
+var today = dayjs(); 
+
+// Get the save button element
+var saveBtn = document.querySelector(".saveBtn"); 
+
+// Get all time block elements
+var hoursEl = document.querySelectorAll(".time-block"); 
+
 
 
 window.onload = function () {
+  // Load stored data from local storage and populate the text areas
   for (var i = 0; i < localStorage.length; i++) {
     var pulledData = localStorage.key(i);
     $("#" + pulledData).children(".description").val(localStorage.getItem(pulledData));
   };
 
+  // Iterate through each time block element to add appropriate classes based on the current hour
   for (var i = 0; i < hoursEl.length; i++) {
     var hourId = $(hoursEl[i]).attr("id");
     hourId = hourId.split("-")[1];
 
     if (today.hour() > hourId) {
-      $(hoursEl[i]).children(".description").addClass("past");
-    } else if (today.hour() == hourId) {
-      $(hoursEl[i]).children(".description").addClass("present");
-    } else if (today.hour() < hourId) {
+      // Add 'past' class for time blocks in the past
+      $(hoursEl[i]).children(".description").addClass("past"); 
+    }
+
+    // Add 'present' class for the current time block
+    else if (today.hour() == hourId) {
+      $(hoursEl[i]).children(".description").addClass("present"); 
+    } 
+
+     // Add 'future' class for time blocks in the future
+    else if (today.hour() < hourId) {
       $(hoursEl[i]).children(".description").addClass("future");
     }
 
-    console.log("hourId is " + hourId);
-    console.log("current hour is " + today.hour());
-    console.log(today.hour() + 1);
+    // debugging statements 
+    // console.log("hourId is " + hourId);
+    // console.log("current hour is " + today.hour());
+    // console.log(today.hour() + 1);
   }
 }
 
-// display current date and time
+// Display the current date and time in header
 $('#currentDay').text(today.format('MMM D, YYYY, h:mm:ss a'));
 
-
-
-// save text in textarea to local storage
+// Save textarea input to local storage when save button class clicked
 $(".saveBtn").on("click", function () {
   var text = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
 
   localStorage.setItem(time, text);
 
-  console.log(text, time);
+  // debugging statement 
+  // console.log(text, time);
 });
 
-// clear local storage button
+// Clear local storage
 $("#clearStorageBtn").on("click", function () {
   localStorage.clear();
 });
